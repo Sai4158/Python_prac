800483bde1b1045665005d3d95e13f09527ea7ec
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-01 00:36:51 -0400

    Type of in python

--- HelloWorld.py
+++ HelloWorld.py
@@ -8,3 +8,30 @@ b =23
 c = a + b
 print(c)
 
+print("-------------")
+print("-------------")
+
+
+# will print in the same line using a comma
+print("ewfewfwe.","ewfew.")
+
+
+print("-------------")
+print("-------------")
+
+# type of 
+
+name = "Sai"
+age = 233223
+num = 32.2454
+
+print(type(name))
+print(type(age))
+print(type(num))
+
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+
+print("-------------")
+print("-------------")
