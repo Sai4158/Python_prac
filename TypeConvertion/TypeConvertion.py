71fc2329b62f3ebb93d7d38ccd2f1180a7abfabf
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-02 19:18:39 -0400

    Converting Str to int in py

---
+++ TypeConvertion/TypeConvertion.py
@@ -0,0 +1,21 @@
+# type convertion 
+
+a  = 5 # int
+b = 5.53 # float
+
+sum = a + b
+
+print(sum)
+
+#-------------------------
+# mixing int and str 
+
+c  =  5
+d = int("5")
+print(type(d))
+sum1 = c + d
+
+print(sum1)
+
+# <class 'int'>
+# 10
