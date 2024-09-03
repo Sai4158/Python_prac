c7c698b91b48525dd2f431d1664ebb54771f2d67
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-03 01:04:28 -0400

    Will calculate the area of the square

---
+++ sumWithInput/Side.py
@@ -0,0 +1,11 @@
+print("This will calculate the area of the square")
+# make sure to wrap it in int
+
+Side = int(input("enter a Side : "))
+
+sum =  Side * Side
+
+print("Aera = ", sum)
+
+# enter a Side : 10
+# Aera =  100
