7dc145c118627d866df1695238d039c67cc61fb8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-02 23:25:59 -0400

    Added comments

--- Input/input.py
+++ Input/input.py
@@ -1,8 +1,13 @@
 name = input("Enter your name: ")
-
 print("Welcome",name)
+
+# Enter your name: sai
+# Welcome sai
 
 # ----------------
 
 typeName = int(input("enter your Age: "))
 print("your age is ",type(typeName), typeName)
+
+# enter your name: 54
+# Welcome <class 'int'> 54
