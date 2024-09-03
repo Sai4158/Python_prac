30b1cad88605fc58d19baec79f262138fd555375
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-03 00:55:48 -0400

    takes a input and adds to number in py

---
+++ sumWithInput/sim.py
@@ -0,0 +1,8 @@
+print("This program will add 2 numbers")
+# make sure to wrap it in int
+a = int(input("enter a number 1: "))
+b = int(input("enter a number 2: "))
+
+sum =  a + b
+
+print("your answer is: ", sum)
