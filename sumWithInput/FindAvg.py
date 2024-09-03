c4fe2ea1ef5a21fc26b78a73a7ae1799eafec08d
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-03 01:09:00 -0400

    This will avg of 2 floats

---
+++ sumWithInput/FindAvg.py
@@ -0,0 +1,17 @@
+print("This will avg of 2 floats ")
+# make sure to wrap it in float
+a = float(input("enter a number 1: "))
+b = float(input("enter a number 2: "))
+
+sum1 =  a / 210
+sum2 =  b / 2
+sum3 = a/2 + b/2
+
+print("Avg of 1 is = ", sum1)
+print("Avg of 2 is = ", sum2)
+print("added sum of Avg of 2 nums = ", sum3)
+# This will avg of 2 floats 
+# enter a number 1: 54.434
+# enter a number 2: 34.5643
+# Avg of 1 is =  27.217
+# Avg of 2 is =  17.28215
