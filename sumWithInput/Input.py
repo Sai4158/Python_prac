238c18fe18b03f6f0f42cf86c928095666c427c6
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-04 23:08:51 -0400

    Will take input and print it then Type of data it is

---
+++ sumWithInput/Input.py
@@ -0,0 +1,12 @@
+a = int(input("What is ur age: "))
+b = str(input("What where do you live here: "))
+
+
+print(a,  type(a))
+print(b, type(b))
+
+# This will take the input and print it then the Type of data it is 
+# What is ur age: 435
+# What where do you live here: usa   
+# 435 <class 'int'>
+# usa <class 'str'>
