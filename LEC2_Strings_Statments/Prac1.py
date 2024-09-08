06f90ed78c07d4819140138e6e4afa9dc96f97e3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-08 01:47:26 -0400

    WAP to user input first name and print its length

---
+++ LEC2_Strings_Statments/Prac1.py
@@ -0,0 +1,8 @@
+# WAP to user input first name and print its length
+
+a =  input("Enter you first name please: ")
+# make sure this is , not +
+print("The length of your first name is: ", len(a))
+
+# Enter you first name please: I am Sai 
+# The length of your first name is:  9
