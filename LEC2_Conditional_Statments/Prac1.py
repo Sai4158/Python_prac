2bd53b48db9a7f273e85a904ca295448ddd72a3c
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-08 12:52:05 -0400

    WAP a program to check if entered num by user is odd or even

---
+++ LEC2_Conditional_Statments/Prac1.py
@@ -0,0 +1,14 @@
+# WAP a program to check if entered num by user is odd or even!
+
+num = int(input("Please enter a number: "))
+
+if(num % 2 == 0):
+    print("This is even number!")
+else:
+    print("This is ODD number")
+
+# Please enter a number: 54
+# This is even number!
+
+# Please enter a number: 55
+# This is ODD number
