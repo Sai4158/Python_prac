c1bd3756162901e9151ba3eeb4644c75aca5bf44
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-08 12:54:40 -0400

    WAP a program to check if entered num is divsible 7

---
+++ LEC2_Conditional_Statments/Prac2.py
@@ -0,0 +1,14 @@
+# WAP a program to check if entered num is divsible 7
+
+num = int(input("Please enter a number: "))
+
+if(num % 7 ==0):
+    print("Yes this is divisible by 7")
+else:
+    print("Nope not divisible")
+
+# Please enter a number: 15
+# Nope not divisible
+
+# Please enter a number: 63
+# Yes this is divisible by 7
