81f20a7c16f23f2f4d42d2a5af8e685a23ad734a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-08 02:03:59 -0400

    if elif else in py

---
+++ LEC2_Conditional_Statments/Ifelse.py
@@ -0,0 +1,14 @@
+# driving licence using the if else 
+
+age = int(input("Please enter age to check for your status: "))
+
+if(age >= 18):
+    print("you are good to go")
+    print("you can apply for licence and vote")
+
+# else if
+elif(age < 18):
+    print("please wait till 18")
+
+else:
+    print("you are a free bird")
