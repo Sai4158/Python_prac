a9a9adcd008e24f917a41afced55bc453da8003e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-08 01:50:31 -0400

    WAP a program to find the $ occurrence in a string

---
+++ LEC2_Strings_Statments/Prac2.py
@@ -0,0 +1,8 @@
+# WAP a program to find the $ occurence in a string 
+# make sure to use a count 
+
+str = "I like $$$ $$$ $$$ $$$$$$4"
+
+print("this is how many times u have type the $: ",str.count("$"))
+
+# this is how many times u have type the $:  15
