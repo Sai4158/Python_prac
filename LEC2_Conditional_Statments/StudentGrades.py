396bedb324a8ae1babeda16f0ad970627b6d4f3b
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-08 02:29:55 -0400

    Student grade system in python using the conditional statments

---
+++ LEC2_Conditional_Statments/StudentGrades.py
@@ -0,0 +1,18 @@
+# this will use if elif for the grades 
+
+grade = int(input("Enter your grade please: "))
+
+if(grade >=95):
+    print("A+")
+elif(grade >=90):
+    print("A-")
+elif(grade >=95):
+    print("A+")
+elif(grade >=85):
+    print("B+")
+elif(grade >=80):
+    print("B")
+elif(grade >=75):
+    print("c")
+else:
+    print("Failed :(")
