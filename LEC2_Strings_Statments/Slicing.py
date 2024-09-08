34c6dba8de64bc63e22b9cebd051721fc6921d00
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-08 00:51:41 -0400

    Slicing in py

---
+++ LEC2_Strings_Statments/Slicing.py
@@ -0,0 +1,19 @@
+# slicing in py 
+
+ab = str("Slicing")
+
+# this is slicing
+# it will print index 2 & 3 only
+print(ab[2:4])
+# ic 
+
+
+# another one 
+dd = str("range rover")
+
+sliceing = dd[5:]
+
+print(sliceing)
+#  rover
+
+
