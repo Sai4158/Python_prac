e0686fbb325cf2bf2519126178ec4f2d25b24af1
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-08 13:38:40 -0400

    this will compare values and the print the greatest value

---
+++ LEC2_Conditional_Statments/Prac3.py
@@ -0,0 +1,13 @@
+# print the greatest num 
+a =545
+b =3 
+c =7
+
+if(a>b and a>c):
+    print(a)
+elif(b>a and b>c):
+    print(b)
+else:
+    print(c)
+
+# this will compare values and the print the greatest value 
