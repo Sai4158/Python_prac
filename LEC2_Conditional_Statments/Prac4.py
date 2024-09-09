e155a572d88405dfd62351885b9f4c67df68e57a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-09 00:22:59 -0400

    WAP when user enter a car name and if its good car then say nice car

---
+++ LEC2_Conditional_Statments/Prac4.py
@@ -0,0 +1,16 @@
+# WAP when user enter a car name and if its good car then say nice car
+
+car = str(input("Enter you car please: "))
+
+if(car == "bmw"):
+    print("Damn you have nice car")
+elif(car == "audi"):
+    print("You got a audi thats fast")
+else:
+    print("you got a slow car sorry!")
+
+# Enter you car please: bmw
+# Damn you have nice car
+
+# Enter you car please: honda
+# you got a slow car sorry!
