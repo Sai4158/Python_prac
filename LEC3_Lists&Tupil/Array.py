a291032cc091c38269b6eb26aaad2eba74ad03d8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-14 01:56:01 -0400

    Length in py

--- LEC3_Lists&Tupil/Array.py
+++ LEC3_Lists&Tupil/Array.py
@@ -8,6 +8,9 @@ print(type(marks))
 # {34.6, 435.6, 4.76, 5.54, 56.7}
 # <class 'list'>
 
+# this will print the length
+print("length is: ", len(marks))
+# length is:  5
 
 # printing the index 
 print(marks[0])
