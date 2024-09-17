5a270f1e642c97b87d36b20c5a7fb7efff82f59b
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-17 00:32:21 -0400

    Also added the sort function

--- LEC3_Lists&Tupil/Prac3.py
+++ LEC3_Lists&Tupil/Prac3.py
@@ -1,6 +1,6 @@
 # wap to count the number of students with the a grade in the following tuple
 
-list  = ["a","f","f"]
+list  = ["a","a","c","b","c","f","f"]
 
 a   =  list.count("f")
 
@@ -9,5 +9,7 @@ print(a)
 # there are 2 "f" since we are asking to count the word the "f"
 
 
+list.sort()
 
-
+print(list)
+# ['a', 'a', 'b', 'c', 'c', 'f', 'f']
