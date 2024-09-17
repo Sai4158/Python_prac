f4421bc6e85ffb38488594d592d54209854d9242
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-17 00:30:53 -0400

    wap to count the number of students with the a grade

---
+++ LEC3_Lists&Tupil/Prac3.py
@@ -0,0 +1,13 @@
+# wap to count the number of students with the a grade in the following tuple
+
+list  = ["a","f","f"]
+
+a   =  list.count("f")
+
+print(a)
+
+# there are 2 "f" since we are asking to count the word the "f"
+
+
+
+
