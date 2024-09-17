3d2f59f607acbe840dfc6528305a00c04bf525c5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-17 00:16:54 -0400

    WAP to append values from the input and print it

---
+++ LEC3_Lists&Tupil/prac1.py
@@ -0,0 +1,20 @@
+# [] is the empty list 
+list = [] 
+# storing the values in the a 
+a = input("Please enter movie 1: ")
+b = input("Please enter movie 2: ")
+c = input("Please enter movie 3: ")
+
+# append the a b c to the list 
+list.append(a)
+list.append(b)
+list.append(c)
+
+# prints the values
+print (list)
+
+
+# Please enter movie 1: sai
+# Please enter movie 2: abhi
+# Please enter movie 3: pranvac
+# ['sai', 'abhi', 'pranvac']
