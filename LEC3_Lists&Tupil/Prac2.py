e2f95fc58585328525c7914762554a72869cdf20
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-17 00:24:34 -0400

    Wap to check if list a panlindrome

---
+++ LEC3_Lists&Tupil/Prac2.py
@@ -0,0 +1,12 @@
+# wap to check if a list contains a panlindrome of elements 
+list1 = [1,2,3]
+list2  = [1,2,3]
+
+copyList = list2.copy()
+
+copyList.reverse
+
+if(list1 == list2):
+    print("This is a palindrome")
+else:
+    print("This is not a palindrome")
