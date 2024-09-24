a5a7ba3efb26f99dfc6403139552c2fbbc2c00d3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-24 01:02:45 -0400

    Collection add and remove

---
+++ LEC4_Dictionary&Set/Collection.py
@@ -0,0 +1,20 @@
+collection  =  set()
+
+print(type(collection))
+
+# <class 'set'>
+# ---------------------
+
+collection.add(1)
+collection.add(2)
+
+print(collection)
+# {1, 2}
+
+
+# -----------------
+# This will remove the collections 
+collection.remove(1)
+print(collection)
+# {1}
+
