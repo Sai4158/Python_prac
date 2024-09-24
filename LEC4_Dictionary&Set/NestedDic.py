d5cc828d71782ad3622dd7e5ff0163db66599d67
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-23 23:35:49 -0400

    Nested Dic in py

---
+++ LEC4_Dictionary&Set/NestedDic.py
@@ -0,0 +1,13 @@
+student = {
+    "name" : "rahul kumar",
+    # This is nested dic
+    "subject" : {
+        "py" : 74,
+        "ewf" : 45,
+        "ewf" : 34,
+    }
+}
+
+print(student)
+
+# {'name': 'rahul kumar', 'subject': {'py': 74, 'ewf': 34}}
