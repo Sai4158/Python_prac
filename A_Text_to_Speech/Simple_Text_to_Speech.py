d7667ac6f9024e07d8a4b7d2016129d512fb01d8
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-28 02:05:20 -0400

    Made simple text to speech in py

---
+++ A_Text_to_Speech/Simple_Text_to_Speech.py
@@ -0,0 +1,18 @@
+from playsound import playsound
+from gtts import gTTS
+
+audio = "spa.mp3"
+
+# lets play the text now using the playsound
+playsound(audio)
+
+sp = gTTS(text="Welcome to the spa, we offer lot of services", lang="en", slow=False)
+
+
+sp.save(audio)
+
+
+
+# Text is now playing
+print("Text is now playing")
+
