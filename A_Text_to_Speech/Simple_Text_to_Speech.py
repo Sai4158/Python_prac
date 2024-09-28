9ff0725fa847d72fdee45e194799c420d9a51026
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-28 02:10:14 -0400

    Text to speech under 10 lines in py

--- A_Text_to_Speech/Simple_Text_to_Speech.py
+++ A_Text_to_Speech/Simple_Text_to_Speech.py
@@ -1,10 +1,7 @@
-
-
-
 from text_to_speech import save
 
-text = "Hello, World!"
-language = "en"  # Specify the language (IETF language tag)
-output_file = "hello_world.mp3"  # Specify the output file (only accepts .mp3)
+text = "This is a slow speech example."
+language = "en"
+output_file = "slow_speech.mp3"
 
-save(text, language, file=output_file)
+save(text, language, slow=True, file=output_file)
