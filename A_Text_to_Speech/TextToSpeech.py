6f737d26cbcc0647e5b84d41c4f183f907792394
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-09-28 01:54:06 -0400

    Saved and playing the text to speech

--- A_Text_to_Speech/TextToSpeech.py
+++ A_Text_to_Speech/TextToSpeech.py
@@ -1,7 +1,18 @@
 from gtts import gTTS
 from playsound import playsound
 
+# audio file
 audio = "speech.mp3"
+
+# choose the lan and pass in
 language = "en"
 
-sp = gTTS(text="Hello World, I am Sai ")
+# Create function for the words 
+sp = gTTS(text="Hello World, I am Sai ", lang=language, slow=False)
+
+# Save into the audio file 
+sp.save(audio)
+
+
+# playsoud using the text function
+playsound(audio)
