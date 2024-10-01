56f0c8f63bc7091380af7736f5a2bb5ece68834e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-01 02:10:42 -0400

    Storing values as dic

---
+++ LEC4_Dictionary&Set/Prac3.py
@@ -0,0 +1,16 @@
+# wap to enter subjects in key and values pair and add them to a list 
+
+marks = {}
+
+x =  str(input("Please enter data for subject- java"))
+marks.update({"java" : x})
+
+x1 =  str(input("Please enter data for subject- python"))
+marks.update({"python" : x1})
+
+x3 =  str(input("Please enter data for subject- c"))
+marks.update({"x" : x3})
+
+
+print("Here are the keys : ", x, x1, x3)
+# Here are the keys :  javasqws edwqdqwd wdqd
