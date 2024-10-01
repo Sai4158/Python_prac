e088b8323f7dbf18b8ea9c66623a6689ae0cf8b5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-01 02:04:37 -0400

    remove dupes from the array

---
+++ LEC4_Dictionary&Set/Prac2.py
@@ -0,0 +1,10 @@
+# remove dupes from the array 
+
+subject = {
+    "python","java","html", "python", "c++", "JavaScript","java", "c++"
+}
+
+a = set(subject)
+
+print(a)
+# {'python', 'java', 'c++', 'JavaScript', 'html'}
