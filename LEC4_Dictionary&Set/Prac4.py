ca49233b90f27eab20a85f8aa9dbd849e92cd2ae
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-04 02:34:45 -0400

    Python recap

--- LEC4_Dictionary&Set/Prac4.py
+++ LEC4_Dictionary&Set/Prac4.py
@@ -4,3 +4,19 @@ values = {
 }
 
 print(values)
+
+
+
+# This is will print dic
+
+hi ={
+    "h1":"hello","h2":"hey"
+}
+
+print(hi)
+
+subject = {
+    "python","java","html", "python", "c++", "JavaScript","java", "c++"
+}
+
+a = set(subject)
