ce2019605e656638976db4e72a5582104779179e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-24 02:06:49 -0400

    Number with def / function

--- Functions/Fun.py
+++ Functions/Fun.py
@@ -1,7 +1,16 @@
 def greet(name):
     print(f"Hello, {name}!")
 
-
-
 greet("sai")
 # Hello, sai!
+
+
+
+# ---------------------
+
+def add_numbers(a, b):
+    result = a + b
+    print(result) 
+
+add_numbers(3,5)
+# 8
