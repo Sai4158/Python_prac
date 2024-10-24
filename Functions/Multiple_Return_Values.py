d69f1c1cf87a4d4dbf19b6f719e69be254f28a7a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-24 02:09:20 -0400

    Multiple Return Values in py using def

---
+++ Functions/Multiple_Return_Values.py
@@ -0,0 +1,9 @@
+def sum_and_product(a, b):
+    sum_result = a + b
+    product_result = a * b
+    return sum_result, product_result
+
+# Test the function
+sum_result, product_result = sum_and_product(5, 10)
+# Print f = formating
+print(f"Sum: {sum_result}, Product: {product_result}")
