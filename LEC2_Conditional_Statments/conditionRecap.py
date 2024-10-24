a053893a082c033ebc20a04ceb586accec0236b5
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-24 02:03:55 -0400

    Using functions and condtional statments made a simple game

--- LEC2_Conditional_Statments/conditionRecap.py
+++ LEC2_Conditional_Statments/conditionRecap.py
@@ -1,6 +1,23 @@
-a =  int(input("Enter a number"))4
+import random
 
-if(a<9):
-    print(a+1)
-else:
-    print(">10")
+def guessing_game():
+    number_to_guess =  random.randint(0,10)
+    attempts = 0
+
+
+    print("Welcome to the Number Guessing Game!")
+    print("I have selected a number between 1 and 100. Try to guess it!")
+
+    while True:
+        user_guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if user_guess < number_to_guess:
+            print("Too low! Try again.")
+        elif user_guess > number_to_guess:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You've guessed the number {number_to_guess} in {attempts} attempts.")
+            break
+
+guessing_game()
