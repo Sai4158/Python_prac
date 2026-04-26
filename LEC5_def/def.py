080d87504e51a8f4014fbf961b1b41d618334b10
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2026-04-25 22:31:59 -0400

    TUPLE, LIST, SET, DIC

--- LEC5_def/def.py
+++ LEC5_def/def.py
@@ -7,3 +7,13 @@ def test():
 
 print("Hello, World!")
 test()
+
+hi = (2,3,4,5,6)
+hi1 = [2,3,4,5,6]
+hi2 = {2,3,4,5,6}
+hi3 = {1: 'one', 2: 'two', 3: 'three'}
+
+print(type(hi))
+print(type(hi1))
+print(type(hi2))
+print(type(hi3))
